19ff787791575e32b04a48432139313b6f45737d
Jake Mays <jake.m.mays@gmail.com>
2017-10-22 08:43:25 -0600

    Gotta be kidding me

--- mix.exs
+++ mix.exs
@@ -2,8 +2,8 @@ defmodule ShapeShift.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ShapeShift,
-     version: "0.1.2",
+    [app: :shape_shift,
+     version: "0.1.3",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
